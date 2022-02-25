d2842715962ddc5a942c76484626ee059553617e
JESUS ANGEL CEPEDA RODRIGUEZ <jesusangelcepedarodriguez60@gmail.com>
2022-02-25 14:38:57 -0600

    Agregar archivos de proyecto.

---
+++ Programa01_05/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Programa01_05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.gboxOperaciones = new System.Windows.Forms.GroupBox();
+            this.rbDivision = new System.Windows.Forms.RadioButton();
+            this.rbMultiplicasion = new System.Windows.Forms.RadioButton();
+            this.rbResta = new System.Windows.Forms.RadioButton();
+            this.rbSuma = new System.Windows.Forms.RadioButton();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.gboxOperaciones.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(41, 12);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 0;
+            this.txtA.Text = "0";
+            this.txtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(41, 53);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 1;
+            this.txtB.Text = "0";
+            this.txtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(196, 12);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // gboxOperaciones
+            // 
+            this.gboxOperaciones.Controls.Add(this.rbDivision);
+            this.gboxOperaciones.Controls.Add(this.rbMultiplicasion);
+            this.gboxOperaciones.Controls.Add(this.rbResta);
+            this.gboxOperaciones.Controls.Add(this.rbSuma);
+            this.gboxOperaciones.Location = new System.Drawing.Point(41, 151);
+            this.gboxOperaciones.Name = "gboxOperaciones";
+            this.gboxOperaciones.Size = new System.Drawing.Size(135, 166);
+            this.gboxOperaciones.TabIndex = 3;
+            this.gboxOperaciones.TabStop = false;
+            this.gboxOperaciones.Text = "Operaciones";
+            // 
+            // rbDivision
+            // 
+            this.rbDivision.AutoSize = true;
+            this.rbDivision.Location = new System.Drawing.Point(6, 135);
+            this.rbDivision.Name = "rbDivision";
+            this.rbDivision.Size = new System.Drawing.Size(54, 17);
+            this.rbDivision.TabIndex = 3;
+            this.rbDivision.Text = "Dividir";
+            this.rbDivision.UseVisualStyleBackColor = true;
+            // 
+            // rbMultiplicasion
+            // 
+            this.rbMultiplicasion.AutoSize = true;
+            this.rbMultiplicasion.Location = new System.Drawing.Point(6, 88);
+            this.rbMultiplicasion.Name = "rbMultiplicasion";
+            this.rbMultiplicasion.Size = new System.Drawing.Size(89, 17);
+            this.rbMultiplicasion.TabIndex = 2;
+            this.rbMultiplicasion.Text = "Multiplicacion";
+            this.rbMultiplicasion.UseVisualStyleBackColor = true;
+            // 
+            // rbResta
+            // 
+            this.rbResta.AutoSize = true;
+            this.rbResta.Location = new System.Drawing.Point(6, 54);
+            this.rbResta.Name = "rbResta";
+            this.rbResta.Size = new System.Drawing.Size(53, 17);
+            this.rbResta.TabIndex = 1;
+            this.rbResta.Text = "Resta";
+            this.rbResta.UseVisualStyleBackColor = true;
+            // 
+            // rbSuma
+            // 
+            this.rbSuma.AutoSize = true;
+            this.rbSuma.Checked = true;
+            this.rbSuma.Location = new System.Drawing.Point(6, 16);
+            this.rbSuma.Name = "rbSuma";
+            this.rbSuma.Size = new System.Drawing.Size(52, 17);
+            this.rbSuma.TabIndex = 0;
+            this.rbSuma.TabStop = true;
+            this.rbSuma.Text = "Suma";
+            this.rbSuma.UseVisualStyleBackColor = true;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(44, 108);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(196, 156);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(112, 161);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 60);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 83);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 120);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 329);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.gboxOperaciones);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gboxOperaciones.ResumeLayout(false);
+            this.gboxOperaciones.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.GroupBox gboxOperaciones;
+        private System.Windows.Forms.RadioButton rbDivision;
+        private System.Windows.Forms.RadioButton rbMultiplicasion;
+        private System.Windows.Forms.RadioButton rbResta;
+        private System.Windows.Forms.RadioButton rbSuma;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
